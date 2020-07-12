152b636b07f1eaa68bdab801ac9ab1f199c7ef08
H <juana.galan@outlook.com>
2020-07-12 22:10:33 +0100

    Adds more unit tests

---
+++ DevOpsBlazor.UnitTests/DevOpsBlazor.UnitTests/CounterPageUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using BlazorDemo.Pages;
+using Microsoft.AspNetCore.Components.Testing;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevOpsBlazor.UnitTests
+{
+    public class CounterPageUnitTests
+    {
+        TestHost host = new TestHost();
+
+        [Fact]
+        public void ShouldRenderCountZero()
+        {
+            var component = host.AddComponent<Counter>();
+
+            // Assert p
+            Assert.Equal("Current count: 0", component.Find("p").InnerText);
+        }
+
+        [Fact]
+        public async Task ShouldIncrementCount()
+        {
+            var component = host.AddComponent<Counter>();
+
+            // Find a button and click.
+            await component.Find("button").ClickAsync();
+            // Assert result.
+            Assert.Equal("Current count: 1", component.Find("p").InnerText);
+        }
+    }
+}
---
+++ DevOpsBlazor.UnitTests/DevOpsBlazor.UnitTests/FetchDataPageUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using BlazorDemo.Pages;
+using Microsoft.AspNetCore.Components.Testing;
+using System;
+using Xunit;
+using static BlazorDemo.Pages.FetchData;
+
+namespace DevOpsBlazor.UnitTests
+{
+    public class FetchDataPageUnitTests
+    {
+        TestHost host = new TestHost();
+
+        [Fact]
+        public void ShouldRenderLoading()
+        {
+            var req = host.AddMockHttp().Capture("");
+
+            var component = host.AddComponent<FetchData>();
+
+            // Assert p
+            Assert.Equal("Loading...", component.Find("p em").InnerText);
+        }
+
+        [Fact]
+        public void ShouldRenderFocusts()
+        {
+            var req = host.AddMockHttp().Capture("");
+
+            var component = host.AddComponent<FetchData>();
+
+            host.WaitForNextRender(() => req.SetResult(new[]
+            {
+                new WeatherForecast { Summary = "First", TemperatureC = 30, Date = DateTime.Now.AddDays(1) },
+                new WeatherForecast { Summary = "Second", TemperatureC = 28, Date = DateTime.Now.AddDays(2) },
+            }));
+
+            Assert.Collection(
+                component.FindAll("tbody tr"),
+                row =>
+                {
+                    Assert.Equal(DateTime.Now.AddDays(1).ToShortDateString(), row.SelectSingleNode("td[1]").InnerText);
+                    Assert.Equal("30", row.SelectSingleNode("td[2]").InnerText);
+                    Assert.Equal((32 + (int)(30 / 0.5556)).ToString(), row.SelectSingleNode("td[3]").InnerText);
+                    Assert.Equal("First", row.SelectSingleNode("td[4]").InnerText);
+                },
+                row => {
+                    Assert.Equal(DateTime.Now.AddDays(2).ToShortDateString(), row.SelectSingleNode("td[1]").InnerText);
+                    Assert.Equal("28", row.SelectSingleNode("td[2]").InnerText);
+                    Assert.Equal((32 + (int)(28 / 0.5556)).ToString(), row.SelectSingleNode("td[3]").InnerText);
+                    Assert.Equal("Second", row.SelectSingleNode("td[4]").InnerText);
+                }
+            );
+        }
+    }
+}
